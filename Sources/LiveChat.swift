10f6523d469c180cef1debd4c71a7f41084ff3f7
alper tabak <alpertabak@teknasyon.com>
2021-12-23 10:04:06 +0300

    Revert "Revert "[feat] SmartPlugs development.""
    This reverts commit 425bf1307451be02bb4ed4ad760b8dba272b3550.

--- Sources/LiveChat.swift
+++ Sources/LiveChat.swift
@@ -17,19 +17,22 @@ public struct LiveChatProperties {
     public let deviceID: String
     public let language: String?
     public let loginCredentials: Credentials?
+	public let smartPlug: SmartPlug?
     
     public init(
         appKey: String,
         host: String,
         deviceID: String,
         language: String? = nil,
-        loginCredentials: Credentials? = nil
+        loginCredentials: Credentials? = nil,
+		smartPlug: SmartPlug? = nil
     ) {
         self.appKey = appKey
         self.host = host
         self.deviceID = deviceID
         self.language = language
         self.loginCredentials = loginCredentials
+		self.smartPlug = smartPlug
     }
 }
 
@@ -77,6 +80,7 @@ public final class Desk360LiveChat {
         
         appCoordinator = AppCoordinator(
             credentials: properites.loginCredentials,
+			smartPlug: properites.smartPlug,
             factory: ProvidersFactory(),
             presenter: viewController
         )
@@ -89,8 +93,13 @@ public final class Desk360LiveChat {
         } else {
             Storage.credentails.delete()
         }
+			
+		if let smartPlugs = properites.smartPlug {
+			try? Storage.smartPlug.save(smartPlugs)
+		} else {
+			Storage.smartPlug.delete()
+		}
        
-        
         appCoordinator?.trigger(.bootstrap)
     }
     
