45987188f6fa11ef84504d9f614d9e9cb6eb6ffa
Ali A. Hilal <al94_200970@yahoo.com>
2022-08-24 16:39:53 +0300

    [fix] Update SPM manifest file

--- Package.swift
+++ Package.swift
@@ -16,11 +16,23 @@ let package = Package(
             targets: ["Desk360LiveChat"]
         ),
     ],
-    dependencies: [],
+    dependencies: [
+
+        .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk.git", from: "8.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.0")),
+        .package(url: "https://github.com/onevcat/Kingfisher.git", .upToNextMajor(from: "7.0.0")),
+        .package(url: "https://github.com/Teknasyon-Teknoloji/PersistenceKit.git", .branch("master")),
+        .package(url: "https://github.com/ninjaprox/NVActivityIndicatorView.git")
+    ],
     targets: [
         .target(
             name: "Desk360LiveChat",
-            dependencies: [],
+            dependencies: [
+                 "PersistenceKit", "Alamofire", "NVActivityIndicatorView", "Kingfisher",
+                 .product(name: "FirebaseAuth", package: "Firebase"),
+                 .product(name: "Database", package: "Firebase")
+
+          ],
             path: "Sources"
         ),
         .testTarget(
