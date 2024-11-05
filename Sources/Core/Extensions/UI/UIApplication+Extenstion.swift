9806bcb40d62bbf689f561048ed02759cf523f30
Osman Yıldırım <osmanyildirim@teknasyon.com>
2024-11-05 13:57:01 +0300

    [chore] code format

--- Sources/Core/Extensions/UI/UIApplication+Extenstion.swift
+++ Sources/Core/Extensions/UI/UIApplication+Extenstion.swift
@@ -8,39 +8,33 @@
 import UIKit
 
 extension UIApplication {
-	var statusBarUIView: UIView? {
+    var statusBarUIView: UIView? {
+        if #available(iOS 13.0, *) {
+            let tag = 3848245
 
-		if #available(iOS 13.0, *) {
-			let tag = 3848245
+            let keyWindow = UIApplication.shared
+                                         .connectedScenes
+                                         .compactMap { $0 as? UIWindowScene }
+                                         .first?
+                                         .windows
+                                         .first
 
-			let keyWindow = UIApplication
-				.shared
-				.connectedScenes
-				.compactMap { $0 as? UIWindowScene }
-				.first?
-				.windows
-				.first
-
-			if let statusBar = keyWindow?.viewWithTag(tag) {
-				return statusBar
-			} else {
-				var statusBarFrame = keyWindow?.windowScene?.statusBarManager?.statusBarFrame ?? .zero
+            if let statusBar = keyWindow?.viewWithTag(tag) {
+                return statusBar
+            } else {
+                var statusBarFrame = keyWindow?.windowScene?.statusBarManager?.statusBarFrame ?? .zero
                 statusBarFrame.size.height = keyWindow?.safeAreaInsets.top ?? 0
 
                 let statusBarView = UIView(frame: statusBarFrame)
-				statusBarView.tag = tag
-				statusBarView.layer.zPosition = 999999
+                statusBarView.tag = tag
+                statusBarView.layer.zPosition = 999999
 
-				keyWindow?.addSubview(statusBarView)
-				return statusBarView
-			}
-
-		} else {
-
-			if responds(to: Selector(("statusBar"))) {
-				return value(forKey: "statusBar") as? UIView
-			}
-		}
-		return nil
-	}
+                keyWindow?.addSubview(statusBarView)
+                return statusBarView
+            }
+        } else if responds(to: Selector(("statusBar"))) {
+            return value(forKey: "statusBar") as? UIView
+        }
+        return nil
+    }
 }
